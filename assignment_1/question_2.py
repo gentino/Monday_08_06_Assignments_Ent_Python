ad2ec6ae86cdd7d12aefefc8612c15f840bae34a
Miracle Obike <36830823+gentino@users.noreply.github.com>
2026-06-09 08:58:42 +0100

    Update question_2.py

--- assignment_1/question_2.py
+++ assignment_1/question_2.py
@@ -5,10 +5,10 @@ B = float(input("Enter the value of B: "))
 # Check if A is greater than B
 if A >= B:
     difference = A - B
-    print(f"{A} - {B}  =  difference")
+    print(f"{A} - {B}  =  {difference}")
 
 # Check if B is greater than A
 else:
     difference = B - A
-    print(f" {B} - {A} =  difference")
+    print(f" {B} - {A} =  {difference}")
 
