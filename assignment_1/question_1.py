06d12197205c878a62e0d628387e03b753aa296e
Miracle Obike <36830823+gentino@users.noreply.github.com>
2026-06-09 22:23:48 +0100

    Update question_1.py

--- assignment_1/question_1.py
+++ assignment_1/question_1.py
@@ -1,6 +1,10 @@
-#The program  request for your name
-# And returns 'Good morning  ' year name ' . How are you doing?'
-
-name = input("Whats your name ?: ").strip() #gets user name input
-
-print(f'Good Morning  {name}. How are you doing?') # displays   user's name with greetings
+'''
+This Python program prompts the user to enter their name using the input() function. 
+The .strip() method is used to remove any leading or trailing spaces from the user's input. 
+After receiving the name, the program displays a personalized greeting message using an f-string. 
+The output greets the user with "Good Morning" followed by their name and asks how they are doing.
+'''
+#gets user name input
+name = input("Whats your name ?: ").strip() 
+# displays   user's name with greetings
+print(f'Good Morning  {name}. How are you doing?') 
